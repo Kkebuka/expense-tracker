e979c718a5e9d58fc50befaf21a25b61d68a1b3d
kkebuka <kkebuka@gmail.com>
2024-06-24 18:38:22 +0100

    update to app file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./IncomeExpenses";
+import { TransactionList } from "./TransactionList";
+import { AddTransaction } from "./AddTransaction";
 
 function App() {
-  return <div className="App">Hello word</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
 }
 
 export default App;
